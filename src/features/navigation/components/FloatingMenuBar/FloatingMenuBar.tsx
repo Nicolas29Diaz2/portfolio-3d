135249c50bdfc6eeb5662878cb0be1c9832213d7
Nicolás Díaz <diazsantosnicolas10@gmail.com>
2026-06-12 13:48:53 -0500

    feat(chore): remove products button in ui

--- src/features/navigation/components/FloatingMenuBar/FloatingMenuBar.tsx
+++ src/features/navigation/components/FloatingMenuBar/FloatingMenuBar.tsx
@@ -4,13 +4,11 @@ import { useThemeStore } from "@/store/themeStore";
 import { MenuSvg } from "@/ui/icons/MenuSvg";
 import { SwitchThemeSvg } from "@/ui/icons/SwitchThemeSvg";
 import "./FloatingMenuBar.css";
-import { useNavigate } from "react-router-dom";
 
 export function FloatingMenuBar({ visible }: Readonly<FloatingMenuBarProps>) {
   const sceneTheme = useThemeStore((state) => state.sceneTheme);
   const setSceneTheme = useThemeStore((state) => state.setSceneTheme);
   const setMenuView = useNavigationStore((state) => state.setMenuView);
-  const navigate = useNavigate();
 
   if (!visible) return null;
 
@@ -20,16 +18,6 @@ export function FloatingMenuBar({ visible }: Readonly<FloatingMenuBarProps>) {
   return (
     <header className="floating-menu-bar" aria-label="Floating navigation menu">
       <ul>
-        <li>
-          <button
-            onClick={() => navigate("/products")}
-            aria-label="Go to Products"
-            id="products-button"
-            style={{ color: iconColor }}
-          >
-            Go to Products
-          </button>
-        </li>
         <li>
           <button
             onClick={() =>
